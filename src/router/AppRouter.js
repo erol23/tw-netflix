6bf1777fc50a8637f0bb69ab0c07e3ccea2bd549
erol23 <hurkanrol15@gmail.com>
2023-03-17 19:29:20 +0100

    first commit

---
+++ src/router/AppRouter.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Navbar from '../components/Navbar'
+
+const AppRouter = () => {
+  return (
+    <>
+      <Navbar/>
+      <Routes>
+        <Route />
+      </Routes>
+    </>
+  )
+}
+
+export default AppRouter
